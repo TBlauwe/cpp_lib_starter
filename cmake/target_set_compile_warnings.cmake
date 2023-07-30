36269ef4f87c42165a4981429174d5b8e2bf2559
TBlauwe <tdeblauwe@pm.me>
2023-07-30 11:07:30 +0200

    Improved cmake function "target_set_compile_warnings_cxx" to take scope as arg

--- cmake/target_set_compile_warnings.cmake
+++ cmake/target_set_compile_warnings.cmake
@@ -1,9 +1,14 @@
-function(target_set_compile_warnings_cxx _TARGET)
+function(target_set_compile_warnings_cxx)
+	set(opts)
+	set(one_value_args TARGET SCOPE)
+	set(multi_value_args)
+	cmake_parse_arguments(TARGET_SET_COMPILE_WARNINGS_CXX "${opts}" "${one_value_args}" "${multi_value_args}" ${ARGN})
+
     if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang"
             OR CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang"
             OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU"
     )
-        target_compile_options(${_TARGET} PRIVATE
+        target_compile_options(${TARGET_SET_COMPILE_WARNINGS_CXX_TARGET} ${TARGET_SET_COMPILE_WARNINGS_CXX_SCOPE}
                 #$<$<CONFIG:RELEASE>:-Werror>
                 $<$<CONFIG:Debug>:-Wshadow>
                 $<$<CONFIG:Debug>:-Wunused>
@@ -23,7 +28,7 @@ function(target_set_compile_warnings_cxx _TARGET)
         )
 
     elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-        target_compile_options(${_TARGET} PRIVATE
+        target_compile_options(${TARGET_SET_COMPILE_WARNINGS_CXX_TARGET} ${TARGET_SET_COMPILE_WARNINGS_CXX_SCOPE}
                 #$<$<CONFIG:RELEASE>:/WX>
                 /W4
                 /w14242 /w14254 /w14263
@@ -38,7 +43,5 @@ function(target_set_compile_warnings_cxx _TARGET)
         message(WARNING
                 "No Warnings specified for ${CMAKE_CXX_COMPILER_ID}. "
                 "Consider using one of the following compilers: Clang, GNU, MSVC, AppleClang.")
-
     endif ()
-
 endfunction()
